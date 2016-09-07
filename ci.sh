c1579c605fa565b357396416ac4acd2859676934
leaf corcoran <leafot@gmail.com>
2016-09-06 23:48:53 -0700

    echo, not cat

--- ci.sh
+++ ci.sh
@@ -21,7 +21,7 @@ echo "synchronous_commit = off" >> /var/lib/postgres/data/postgresql.conf
 echo "full_page_writes = off" >> /var/lib/postgres/data/postgresql.conf
 su postgres -c '/usr/bin/pg_ctl -s -D /var/lib/postgres/data start -w -t 120'
 
-cat 'return "test"' > lapis_environment.lua
+echo "return 'test'" > lapis_environment.lua
 
 moonc schema.moon
 moonc config.moon
