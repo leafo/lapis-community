dd308b31f0989dfc03535d0efd529bcab193d334
leaf corcoran <leafot@gmail.com>
2016-09-06 23:45:44 -0700

    switch to running tests in docker

---
+++ ci.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -o xtrace
+
+
+# remove outdated bundled version
+luarocks-5.1 remove --force lapis
+
+luarocks-5.1 install busted
+luarocks-5.1 install https://raw.githubusercontent.com/leafo/lapis/master/lapis-dev-1.rockspec
+luarocks-5.1 install moonscript
+luarocks-5.1 install date
+
+luarocks-5.1 make
+
+# start postgres
+echo "fsync = off" >> /var/lib/postgres/data/postgresql.conf
+echo "synchronous_commit = off" >> /var/lib/postgres/data/postgresql.conf
+echo "full_page_writes = off" >> /var/lib/postgres/data/postgresql.conf
+su postgres -c '/usr/bin/pg_ctl -s -D /var/lib/postgres/data start -w -t 120'
+
+cat 'return "test"' > lapis_environment.lua
+
+moonc schema.moon
+moonc config.moon
+moonc community
+createdb -U postgres community_test
+LAPIS_SHOW_QUERIES=1 luajit -e 'require("schema").make_schema()'
+
+cat $(which busted) | sed 's/\/usr\/bin\/lua5\.1/\/usr\/bin\/luajit/' > busted
+chmod +x busted
+./busted -o utfTerminal
