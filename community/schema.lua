bd34398bfb77c1ef15c97dd4f8861b64fde95de1
leaf corcoran <leafot@gmail.com>
2017-10-24 12:35:41 -0700

    add a way to enforce version we're migrating to

--- community/schema.lua
+++ community/schema.lua
@@ -1,6 +1,9 @@
 local run_migrations
-run_migrations = function()
+run_migrations = function(version)
   local m = require("lapis.db.migrations")
+  if version then
+    assert(m[tonumber(version)], "Expected to migrate to lapis-community version " .. tostring(version) .. " but it was not found. Did you forget to update lapis-community?")
+  end
   return m.run_migrations(require("community.migrations"), "community")
 end
 return {
--- community/schema.moon
+++ community/schema.moon
@@ -1,6 +1,10 @@
 
-run_migrations = ->
+run_migrations = (version) ->
   m = require "lapis.db.migrations"
+
+  if version
+    assert m[tonumber version], "Expected to migrate to lapis-community version #{version} but it was not found. Did you forget to update lapis-community?"
+
   m.run_migrations require("community.migrations"), "community"
 
 { :run_migrations }
