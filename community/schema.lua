8cada0e44e74555ea42ab0d8af01fa5b5971b425
leaf corcoran <leafot@gmail.com>
2017-10-24 12:41:55 -0700

    show available version, fix looking up in wrong table

--- community/schema.lua
+++ community/schema.lua
@@ -1,10 +1,23 @@
 local run_migrations
 run_migrations = function(version)
   local m = require("lapis.db.migrations")
-  if version then
-    assert(m[tonumber(version)], "Expected to migrate to lapis-community version " .. tostring(version) .. " but it was not found. Did you forget to update lapis-community?")
+  local migrations = require("community.migrations")
+  if version and not migrations[tonumber(version)] then
+    local versions
+    do
+      local _accum_0 = { }
+      local _len_0 = 1
+      for key in pairs(migrations) do
+        _accum_0[_len_0] = key
+        _len_0 = _len_0 + 1
+      end
+      versions = _accum_0
+    end
+    table.sort(versions)
+    local available_version = versions[#versions]
+    error("Expected to migrate to lapis-community version " .. tostring(version) .. " but it was not found (have " .. tostring(available_version) .. "). Did you forget to update lapis-community?")
   end
-  return m.run_migrations(require("community.migrations"), "community")
+  return m.run_migrations(migrations, "community")
 end
 return {
   run_migrations = run_migrations
--- community/schema.moon
+++ community/schema.moon
@@ -2,9 +2,14 @@
 run_migrations = (version) ->
   m = require "lapis.db.migrations"
 
-  if version
-    assert m[tonumber version], "Expected to migrate to lapis-community version #{version} but it was not found. Did you forget to update lapis-community?"
+  migrations = require("community.migrations")
 
-  m.run_migrations require("community.migrations"), "community"
+  if version and not migrations[tonumber version]
+    versions = [key for key in pairs migrations]
+    table.sort versions
+    available_version = versions[#versions]
+    error "Expected to migrate to lapis-community version #{version} but it was not found (have #{available_version}). Did you forget to update lapis-community?"
+
+  m.run_migrations migrations, "community"
 
 { :run_migrations }
